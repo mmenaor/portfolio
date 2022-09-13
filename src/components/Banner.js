94bdcc0860b69c356dc76f5c10b0511533469072
mmenaor <mmenaor@gmail.com>
2022-09-13 11:49:12 -0500

    bugs fixed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,44 +11,44 @@ export const Banner = () => {
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(200 - Math.random() * 100);
     // const [index, setIndex] = useState(1);
-    const toRotate = [ "FullStack Developer", "FrontEnd Developer", "BackEnd Developer" ];
     const period = 1500;
-
+    
     useEffect(() => {
+        const tick = () => {
+            const toRotate = [ "FullStack Developer", "FrontEnd Developer", "BackEnd Developer" ];
+            let i = loopNum % toRotate.length;
+            let fullText = toRotate[i];
+            let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    
+            setText(updatedText);
+    
+            if (isDeleting) {
+                setDelta(prevDelta => prevDelta / 1.8);
+            }
+    
+            if (!isDeleting && updatedText === fullText) {
+                setIsDeleting(true);
+                // setIndex(prevIndex => prevIndex - 1);
+                setDelta(period);
+            } else if (isDeleting && updatedText === '') {
+                setIsDeleting(false);
+                setLoopNum(loopNum + 1);
+                // setIndex(1);
+                setDelta(500);
+            } else {
+                // setIndex(prevIndex => prevIndex + 1);
+                if (updatedText.length === 1){
+                    setDelta(200 - Math.random() * 100);
+                }
+            }
+        }
+
         let ticker = setInterval(() => {
             tick();
         }, delta);
 
         return () => { clearInterval(ticker) };
-    }, [text, delta])
-
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-        setText(updatedText);
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 1.8);
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            // setIndex(prevIndex => prevIndex - 1);
-            setDelta(period);
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            // setIndex(1);
-            setDelta(500);
-        } else {
-            // setIndex(prevIndex => prevIndex + 1);
-            if (updatedText.length === 1){
-                setDelta(200 - Math.random() * 100);
-            }
-        }
-    }
+    }, [text, delta, isDeleting, loopNum])
 
     return (
         <section className="banner" id="home">
